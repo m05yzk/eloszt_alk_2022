a94b4f15729cacd1d95a526ca933258373277b26
m05yzk <m05yzk@neptun.gdf.hu>
2022-05-18 14:11:03 +0200

    .

---
+++ KutyApp/src/m05yzk/KutyApp.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package m05yzk;
+
+/**
+ *
+ * @author Win10
+ */
+public class KutyApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
